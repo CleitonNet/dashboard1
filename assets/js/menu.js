96d8b0d09d11d49f9b007efa75913e1af64d6f19
CletionNet <cleiton.net@live.com>
2024-10-28 17:27:43 -0300

    First Commit

---
+++ assets/js/menu.js
@@ -0,0 +1,64 @@
+export class Menu {
+    constructor(menu, openBtn, closeBtn, direction = 'right') {
+        this.menu = document.querySelector(menu);
+        this.openBtn = document.querySelector(openBtn);
+        this.closeBtn = document.querySelector(closeBtn);
+        this.direction = direction;
+        this.toggleIcon = true;
+        this.isOpen = false;
+        if (!this.menu) return;
+        this.menu.style.transition = `opacity 0.3s ease, ${this.direction} 0.3s ease`;
+        this.init();
+    }
+
+    toggleMenuIcon() {
+        if (this.toggleIcon) this.openBtn.innerHTML = `<span class="material-symbols-outlined">${this.isOpen ? 'close' : 'menu'}</span>`;
+    }
+
+    open() {
+        if (this.isOpen) return;
+        this.menu.style.display = 'flex';
+        this.menu.style[this.direction] = `${-this.menu.offsetWidth}px`;
+        setTimeout(() => {
+            this.menu.style.opacity = '1';
+            this.menu.style[this.direction] = '0';
+        }, 10);
+
+        this.isOpen = true;
+        this.toggleMenuIcon();
+    }
+
+    close() {
+        if (!this.isOpen) return;
+        this.menu.style.opacity = '0';
+        this.menu.style[this.direction] = `${-this.menu.offsetWidth}px`;
+        setTimeout(() => this.menu.removeAttribute('style'), 300);
+
+        this.isOpen = false;
+        this.toggleMenuIcon();
+    }
+
+    toggle() {
+        this.isOpen ? this.close() : this.open();
+    }
+
+    closeOnClickOutside(event) {
+        if (!this.menu.contains(event.target) && !this.openBtn.contains(event.target)) this.close();
+    }
+
+    init() {
+        [this.openBtn, this.closeBtn].forEach(btn => btn?.addEventListener('click', (e) => (e.stopPropagation(), this.toggle())));
+        document.addEventListener('click', (e) => this.isOpen && this.closeOnClickOutside(e));
+        document.addEventListener('keydown', (e) => this.isOpen && e.key === 'Escape' && this.close());
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menus = [
+        new Menu('#menu-profile', '#btnProfile', '#btnCloseProfile'),
+        new Menu('#menu-main-responsive', '#openMenuMain', '#closeMenuMain'),
+        new Menu('#aside-left', '#openMenu', '#closeMenu', 'left')
+    ];
+
+    document.addEventListener('keydown', (e) => e.key === 'Escape' && menus.forEach(menu => menu.close()));
+});
